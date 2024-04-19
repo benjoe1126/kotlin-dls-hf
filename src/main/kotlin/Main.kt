a2bb1064a03a315c01aeadb80892c78644943ff2
benjoe1126 <bence.abonyi16@gmail.com>
2024-04-19 11:51:01 +0200

    initial

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,20 @@
+import network.packet.ipv4
+import network.packet.packet
+
+fun main(){
+    try {
+        val packet = packet {
+            srcIpv4 = ipv4{
+                address = "127.0.0.1"
+                mask = 24u
+            }
+            dstIpv4 = ipv4 {
+                address = "192.168.0.1"
+                mask = 8u
+            }
+        }
+        println(packet.dstIpv4)
+    } catch(e: IllegalArgumentException){
+        println(e.message)
+    }
+}
