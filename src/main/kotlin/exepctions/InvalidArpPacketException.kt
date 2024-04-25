c1405afbe56e3dc442dcc61680ffe567941162a9
benjoe1126 <bence.abonyi16@gmail.com>
2024-04-25 21:13:06 +0200

    added new exception class

--- src/main/kotlin/exepctions/InvalidArpPacketException.kt
+++ src/main/kotlin/exepctions/InvalidArpPacketException.kt
@@ -1,4 +1,8 @@
 package exepctions
 
-class InvalidArpPacketException {
+class InvalidArpPacketException: Exception {
+    constructor(): super()
+    constructor(msg: String): super(msg)
+    constructor(msg: String, cause: Throwable): super(msg, cause)
+    constructor(cause:Throwable): super(cause)
 }
