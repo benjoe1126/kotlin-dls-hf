c611c75130c82f2155a1c21a3666415bb14d8461
benjoe1126 <bence.abonyi16@gmail.com>
2024-05-13 21:02:15 +0200

    project finished and ready for hw submission

---
+++ src/main/kotlin/enums/DNSFlags.kt
@@ -0,0 +1,5 @@
+package enums
+
+enum class DNSFlags {
+    AA, TC, RD, RA, Z, AD, CD
+}
--- src/main/kotlin/network/message/Message.kt
+++ src/main/kotlin/network/message/Message.kt
@@ -2,8 +2,4 @@ package network.message
 
 import printable.Printable
 
-class Message: Printable {
-    override fun print(): String {
-        TODO("Not yet implemented")
-    }
-}
+abstract class Message: Printable
